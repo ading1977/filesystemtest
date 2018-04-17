89d129cc65597e959a289b1cdd7f81844148d99e
Meng Ding <dingmeng@gmail.com>
2018-04-17 18:45:26 +0000

    First commit

---
+++ test_fs.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+if [ "$#" -ne 1 ]; then
+  echo Usage: $0  nfs \| gluster
+  exit 1
+fi
+
+fs=$1
+if [ "$fs" != "nfs" -a "$fs" != "gluster" ]; then
+  echo Usage: $0  nfs \| gluster
+  exit 1
+fi
+
+export TZ="America/Toronto"
+
+home=/home/centos
+mkdir -p $home/$fs
+date=`date +%s`
+host=`hostname --short`
+log="$home/$fs/$host.$date.log"
+
+echo ======================================= >> $log 2>&1
+
+mount=/mnt/$fs
+mkdir -p $mount
+echo Mount $mount >> $log 2>&1
+if [ "$fs" = "nfs" ]; then
+  sudo umount -f $mount >> $log 2>&1
+  sudo mount -o vers=4 kube08:/var/nfsshare $mount >> $log 2>&1
+  if [ $? -ne 0 ]; then
+    echo Failed to mount $mount >> $log 2>&1
+    echo ======================================= >> $log 2>&1
+    exit 1
+  fi
+fi
+
+source=$home/gcc-4.9.2
+file=gcc-4.9.2.$host.$date
+dest=$mount/$file
+
+while true
+do
+date >> $log 2>&1
+echo Copy to $dest >> $log 2>&1
+(time cp -rf $source $dest) >> $log 2>&1
+cd $dest
+echo Configure ... >> $log 2>&1
+(time ./configure --disable-multilib --enable-languages=c,c++) >> $log 2>&1
+echo Compile ... >> $log 2>&1
+(time make -j8) >> $log 2>&1
+if [ $? -eq 0 ]; then
+  echo Delete $dest ... >> $log 2>&1
+  cd ..
+  (time rm -rf $dest) >> $log 2>&1
+fi
+date >> $log 2>&1
+done
+echo ======================================= >> $log 2>&1
