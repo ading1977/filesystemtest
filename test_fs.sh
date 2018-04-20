fea63e0c968246abfde37a2b749fbc4072422551
root <root@ip-172-31-27-70.ca-central-1.compute.internal>
2018-04-20 16:59:37 +0000

    Fix bug

--- test_fs.sh
+++ test_fs.sh
@@ -70,11 +70,11 @@ echo Configure ... >> $log 2>&1
 (time ./configure --disable-multilib --enable-languages=c,c++) >> $log 2>&1
 echo Compile ... >> $log 2>&1
 (time make -j8) >> $log 2>&1
-if [ $? -eq 0 ]; then
+if [ $? -ne 0 ]; then
   echo Retry compiling after 10 seconds ... >> $log 2>&1
   sleep 10
   (time make -j8) >> $log 2>&1
-  if [ $? -eq 0 ]; then
+  if [ $? -ne 0 ]; then
     echo Compile failed ... >> $log 2>&1
     exit 1
   fi
